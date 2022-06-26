237cd46c11a72c696e6271f83003cbb929d20cff
leonardo <leonardo.s.deluca@gmail.com>
2022-06-26 15:45:56 -0300

    Exemplo container IOC Spring boot, ComponentScan

--- src/main/java/Application/VendasApplication.java
+++ src/main/java/Application/VendasApplication.java
@@ -4,10 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+/*@ComponentScan(basePackages = {
+        "Application.repository",
+        "Application.service"
+})
+Adiciona os pacotes que devem ser lidos pela aplicação por padrão não precisa ser utilizado pois já utiliza os pacotes
+presetntes no package Application, utiliza-se para bibliotecas externas*/
 @RestController
 public class VendasApplication {
 
