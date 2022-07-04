6d6d0c30659a2fdd654c699c08bfe0d9d886a9cc
leonardo <leonardo.s.deluca@gmail.com>
2022-07-04 18:37:23 -0300

    Delete exemplos cap1

--- src/main/java/Application/VendasApplication.java
+++ src/main/java/Application/VendasApplication.java
@@ -1,44 +1,12 @@
 package Application;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/*import org.springframework.context.annotation.ComponentScan;*/
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-/*@ComponentScan(basePackages = {
-        "Application.repository",
-        "Application.service"
-})
-Adiciona os pacotes que devem ser lidos pela aplicação por padrão não precisa ser utilizado pois já utiliza os pacotes
-presetntes no package Application, utiliza-se para bibliotecas externas*/
-@RestController
 public class VendasApplication {
-    @Value("${application.name}")
-    private String applicationName;
 
-    @cachorro
-    private Animal animal;
-
-    @Bean(name = "ExecutarAnimal")
-    public CommandLineRunner executar(){
-        return args -> {
-          this.animal.fazerBarulho();
-        };
-    }
-
-    @GetMapping("/hello")
-    public String helloWorld()
-    {
-        return applicationName;
-    }
-    public static void main(String[] args) {
-        SpringApplication.run(VendasApplication.class, args);
-    }
+        public static void main(String[] args){
+            SpringApplication.run(VendasApplication.class, args);
+        }
 }
