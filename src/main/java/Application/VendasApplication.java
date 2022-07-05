878f311daa6ba5281e8180351557e017fbe775a8
leonardo <leonardo.s.deluca@gmail.com>
2022-07-04 23:46:57 -0300

    Adicionando métodos salvar cliente e obter todos com jdbcTemplate

--- src/main/java/Application/VendasApplication.java
+++ src/main/java/Application/VendasApplication.java
@@ -1,10 +1,32 @@
 package Application;
 
+import Application.domain.entity.Cliente;
+import Application.domain.repositorio.Clientes;
+import org.h2.command.Command;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class VendasApplication {
+
+        @Bean
+        public CommandLineRunner init(@Autowired Clientes clientes){
+            return args -> {
+                Cliente cliente = new Cliente("Leo");
+                clientes.salvar(cliente);
+
+                Cliente cliente2 = new Cliente("Outro");
+                clientes.salvar(cliente2);
+
+                List<Cliente> todosClientes = clientes.obterTodos();
+                todosClientes.forEach(System.out::println);
+            };
+        }
 
         public static void main(String[] args){
             SpringApplication.run(VendasApplication.class, args);
