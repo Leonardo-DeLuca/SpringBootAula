89a9f66876ec8b6c4c05d94fb3e938cb83074076
leonardo <leonardo.s.deluca@gmail.com>
2022-07-11 10:54:43 -0300

    Inserindo Delet e update de clientes

--- src/main/java/Application/VendasApplication.java
+++ src/main/java/Application/VendasApplication.java
@@ -17,6 +17,7 @@ public class VendasApplication {
         @Bean
         public CommandLineRunner init(@Autowired Clientes clientes){
             return args -> {
+                System.out.println("Salvando Clientes.");
                 Cliente cliente = new Cliente("Leo");
                 clientes.salvar(cliente);
 
@@ -25,6 +26,31 @@ public class VendasApplication {
 
                 List<Cliente> todosClientes = clientes.obterTodos();
                 todosClientes.forEach(System.out::println);
+
+                System.out.println("Atualizando Clientes");
+                todosClientes.forEach(c -> {
+                    c.setNome(c.getNome() + " atualizado");
+                    clientes.atualizar(c);
+                });
+
+                todosClientes = clientes.obterTodos();
+                todosClientes.forEach(System.out::println);
+
+                System.out.println("Buscando clientes");
+                clientes.buscarPorNome("Ou").forEach(System.out::println);
+
+//                System.out.println("Deletando clientes");
+//                clientes.obterTodos().forEach(c -> {
+//                    clientes.deletar(c);
+//                });
+
+                todosClientes = clientes.obterTodos();
+                if (todosClientes.isEmpty()){
+                    System.out.println("Nenhum Cliente Encontrado");
+                }
+                else {
+                    todosClientes.forEach(System.out::println);
+                }
             };
         }
 
